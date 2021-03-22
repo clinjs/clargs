a6deb9a5c3ecccbc87656af3651b1cb9f56ab50d
PierreDemailly <pierredemailly.pro@gmail.com>
2021-03-22 17:42:55 +0100

    add declaration file

---
+++ index.d.ts
@@ -0,0 +1,39 @@
+declare interface Option {
+  name: string;
+  alias: string;
+  description: string;
+}
+
+declare interface Command {
+  name: string;
+  description: string;
+  options: Option[];
+}
+
+declare type Options = Option[];
+declare type Commands = Command[];
+
+declare interface SetupOptions {
+  usage: string;
+  options: Options;
+  commands: Commands;
+}
+
+declare interface OnCommandFailed {
+  failed: boolean;
+}
+
+declare interface OnCommand {
+  so: (cb: any) => OnCommandFailed;
+}
+
+declare module "nezparser" {
+  args: [];
+  options: Options;
+  commands: Comamnds;
+  function setup(options: SetupOption): void;
+  function parse(): void;
+  function on(): OnCommand;
+  function help(): void;
+  function hasOption(option: string, alias: string): boolean
+}
